7b87dbe28d87ad7e2b4eab281810f259a123e865
Tomivav <tomivav@gmail.com>
2016-03-01 10:05:24 +0100

    Initial commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'User Zero',
+            'email' => 'user0@app.com',
+            'password' => bcrypt('user0'),
+            'type' => 0
+        ]);
+
+        User::create([
+            'name' => 'User One',
+            'email' => 'user1@app.com',
+            'password' => bcrypt('user1'),
+            'type' => 1
+        ]);
+
+        User::create([
+            'name' => 'User Two',
+            'email' => 'user2@app.com',
+            'password' => bcrypt('user2'),
+            'type' => 2
+        ]);
+
+        User::create([
+            'name' => 'User Three',
+            'email' => 'user3@app.com',
+            'password' => bcrypt('user3'),
+            'type' => 3
+        ]);
+    }
+}
---
+++ resources/views/main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Aplikacija za upravljanje službenim putovanjima</title>
+        <link rel="stylesheet" href="node_modules/angular-material/angular-material.css">
+    </head>
+
+    <body ng-app="diplomski" ng-cloak>
+        <div ui-view></div>
+
+        <script src="node_modules/angular/angular.js"></script>
+        <script src="node_modules/angular-animate/angular-animate.js"></script>
+        <script src="node_modules/angular-aria/angular-aria.js"></script>
+        <script src="node_modules/angular-material/angular-material.js"></script>
+        <script src="node_modules/angular-ui-router/release/angular-ui-router.js"></script>
+        <script src="node_modules/restangular/node_modules/lodash/lodash.js"></script>
+        <script src="node_modules/restangular/dist/restangular.js"></script>
+        <script src="scripts/app.js"></script>
+    </body>
+
+</html>
